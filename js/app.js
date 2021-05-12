14089fc3a2bf5d9638dc60d438d2183bf890fbfc
Fajar Hidayatullah <69250754+Fajarsch@users.noreply.github.com>
2021-05-12 19:33:08 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,15 @@
 document.getElementById("liveToastBtn").onclick = function() {
     var toastElList = [].slice.call(document.querySelectorAll('.toast'))
     var toastList = toastElList.map(function (toastEl) {
-    return new bootstrap.Toast(toastEl)
+        return new bootstrap.Toast(toastEl)
     });
     toastList.forEach(toast => toast.show()); // This show them
+}
+
+document.getElementById("tooltip-role").onmouseenter = function() {
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+    });
+    tooltipList.forEach(tooltip => tooltip.show());
 }
